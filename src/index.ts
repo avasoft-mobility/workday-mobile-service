0812029eeb50c9bfeaabfa9680393072e4e3efb8
surya-r13 <96774365+surya-r13@users.noreply.github.com>
2022-09-01 17:49:09 +0530

    Configure mobile controller to get mobile app version (#1)

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { json } from "body-parser";
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import { mobileRouter } from "./routers/MobileAppController";
+
+const app = express();
+
+app.use(json());
+
+app.get("/", (request: Request, response: Response) => {
+  return response.send("Mobile backend is healthy");
+});
+
+app.use("/mobile", mobileRouter);
+
+mongoose.connect("mongodb://localhost:27017", () => {
+  console.log("DB is connected");
+});
+
+app.listen(3000, () => {
+  console.log("server listening to port 3000");
+});
