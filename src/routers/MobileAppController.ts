c44ebe46888adeb3b19b2bce698dd242bc2c9dce
surya-r13 <96774365+surya-r13@users.noreply.github.com>
2022-09-02 15:17:59 +0530

    Add POST method to update or create mobile version (#2)

--- src/routers/MobileAppController.ts
+++ src/routers/MobileAppController.ts
@@ -20,13 +20,54 @@ router.get("/users", (req, res) => {
   return res.send({ message: "Users Service is working fine" });
 });
 
+router.post("/versions", async (req: Request, res: Response) => {
+  try {
+    
+    if (!req.query["appId"] || req.query["appId"] === "") {
+      return res.status(401).json({ message: "app id is required." });
+    }
+
+    if (!req.body["latestversion"] || req.body["latestversion"] === "") {
+      return res.status(401).json({ message: "latest version is required" });
+    }
+
+    if (req.body["isrequired"] === undefined) {
+      return res
+        .status(401)
+        .json({ message: "parameter 'isrequired' must be passed" });
+    }
+    const appid = req.query["appId"].toString();
+
+    const Updatedversion = await updateMobileVerison(
+      appid,
+      req.body["latestversion"].toString(),
+      req.body["isrequired"] as boolean
+    );
+
+    if(!Updatedversion)
+    {
+      return res
+      .status(404)
+      .json({ message: "No creation or updation happend" });
+    }
+
+    return res.status(200).json({ data: Updatedversion });
+  } catch (error) {
+    Rollbar.error(error as unknown as Error, req);
+    return res
+      .status(500)
+      .json({ message: (error as unknown as Error).message });
+  }
+});
+
+
 router.get("/versions", async (req: Request, res: Response) => {
   try {
     const mobileVersion = await MobileVersionDb.findOne({
       appid: req.query["appId"],
     });
 
-    if (mobileVersion === null) {
+    if (mobileVersion===null) {
       return res
         .status(404)
         .json({ message: "No result available for this app ID" });
@@ -40,5 +81,25 @@ router.get("/versions", async (req: Request, res: Response) => {
       .json({ message: (error as unknown as Error).message });
   }
 });
+
+const updateMobileVerison = async (
+  appid: string,
+  latestversion: string,
+  isrequired: boolean
+) => {
+  var newItem = {
+    appid: appid,
+    latestversion: latestversion,
+    isrequired: isrequired,
+  };
+  var queryResult = await MobileVersionDb.find({
+    appid: newItem.appid,
+  });
+  if (queryResult.length !== 0) {
+    return await MobileVersionDb.findOne({ appid: appid }).updateOne(newItem);
+  } else {
+    return await MobileVersionDb.create(newItem);
+  }
+};
 
 export { router as mobileRouter };
